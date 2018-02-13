27a5a74d61ad1a2753ec0dd1d6d9f1ea8a92f2ef
NadzeyaZhukovich <naduva0505@gmail.com>
2018-02-13 22:49:39 +0100

    Refactor app.js file

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 var tasks = ["first", "second"];
 
 window.addEventListener("load", function(){
+    var list = document.getElementById("list");
     for(var i = 0; i < tasks.length; i++){
-        console.log(tasks[i]);
-
-        var list = document.getElementById("list");
-
-        var item = document.createElement("li");
-        item.appendChild(document.createTextNode(tasks[i]));
-
-        list.appendChild(item);
+        showTask(list, tasks[i]);
     }
 });
+
+function showTask(list, task) {
+    var item = document.createElement("li");
+    item.appendChild(document.createTextNode(task));
+    list.appendChild(item);
+}
