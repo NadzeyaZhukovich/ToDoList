14934fa5d58382a3d36efcb51e54c9c02521ef27
NadzeyaZhukovich <naduva0505@gmail.com>
2018-02-16 23:21:44 +0100

    Added functionality for deleting the task

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var tasks = ["first", "second"];
+var tasks = ["first"];
 var list = document.getElementById("list");
 
 window.addEventListener("load", function(){
@@ -16,6 +16,18 @@ document.getElementById("button").addEventListener("click", function(){
 
 function showTask(list, task) {
     var item = document.createElement("li");
+    item.setAttribute('id', 'task'+tasks.length);
+    var removeButton = document.createElement("button");
+    removeButton.innerHTML = "delete";
+    removeButton.className = "DeleteButton";
+    removeButton.setAttribute('onClick', 'remove("' + 'task'+tasks.length+'")');
+    removeButton.type = "button";
     item.appendChild(document.createTextNode(task));
+    item.appendChild(removeButton);
     list.appendChild(item);
+}
+
+function remove(id) {
+    var item = document.getElementById(id);
+    list.removeChild(item);
 }
